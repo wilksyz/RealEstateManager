057a85ca926667a49e2431b26d597f75471ca214
wilksyz <antoine-p@hotmail.com>
2019-01-14 15:28:11 +0100

    Second bug resolution

--- app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void configureTextViewQuantity(){
-        int quantity = Utils.convertDollarToEuro(100);
+        String quantity = String.valueOf(Utils.convertDollarToEuro(100));
         this.textViewQuantity.setTextSize(20);
         this.textViewQuantity.setText(quantity);
     }
