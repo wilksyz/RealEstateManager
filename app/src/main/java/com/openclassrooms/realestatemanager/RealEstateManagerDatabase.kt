cd9343509adc8244b693c7f8a25d16486fb27d41
wilksyz <antoine-p@hotmail.com>
2019-01-17 14:42:02 +0100

    Add RealEstateManagerDatabase class

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/RealEstateManagerDatabase.kt
@@ -0,0 +1,42 @@
+package com.openclassrooms.realestatemanager
+
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.RoomDatabase
+import android.arch.persistence.room.OnConflictStrategy
+import android.content.ContentValues
+import android.arch.persistence.db.SupportSQLiteDatabase
+import android.arch.persistence.room.Room
+import android.content.Context
+
+
+@Database(entities = [Property::class], version = 1)
+abstract class RealEstateManagerDatabase : RoomDatabase() {
+
+    @Volatile
+    private var INSTANCE: RealEstateManagerDatabase? = null
+    abstract fun propertyDao(): PropertyDao
+
+    fun getInstance(context: Context): RealEstateManagerDatabase {
+        if (INSTANCE == null) {
+            synchronized(RealEstateManagerDatabase::class.java) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.applicationContext,
+                            RealEstateManagerDatabase::class.java, "Property.db")
+                            .addCallback(prepopulateDatabase())
+                            .build()
+                }
+            }
+        }
+        return INSTANCE!!
+    }
+
+    private fun prepopulateDatabase(): RoomDatabase.Callback {
+        return object : RoomDatabase.Callback() {
+
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+
+            }
+        }
+    }
+}
