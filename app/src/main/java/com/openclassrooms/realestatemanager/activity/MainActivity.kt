3df37dfdaf1f780be2234604c67bce495f2c292f
wilksyz <antoine-p@hotmail.com>
2019-01-21 12:04:03 +0100

    Add DetailsPropertyFragment and implements mode landscape

--- app/src/main/java/com/openclassrooms/realestatemanager/activity/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/activity/MainActivity.kt
@@ -1,47 +1,23 @@
 package com.openclassrooms.realestatemanager.activity
 
-import android.arch.lifecycle.Observer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import android.arch.lifecycle.ViewModelProviders
-import android.util.Log
 import com.facebook.stetho.Stetho
 import com.openclassrooms.realestatemanager.*
+import com.openclassrooms.realestatemanager.fragment.DetailsPropertyFragment
 import com.openclassrooms.realestatemanager.fragment.MainFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private var textViewMain: TextView? = null
-    private var textViewQuantity: TextView? = null
-    private var itemViewModel: PropertyViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Stetho.initializeWithDefaults(this)
 
-        this.textViewMain = findViewById(R.id.activity_main_activity_text_view_main)
-        this.textViewQuantity = findViewById(R.id.activity_main_activity_text_view_quantity)
-
         this.configureFragment()
-        this.configureTextViewMain()
-        this.configureTextViewQuantity()
-        configureViewModel()
-        //createProperty(Property("Manoir",650_000,300,27))
+        this.configureDetailsPropertyFragment()
 
-        getProperties()
-    }
-
-    private fun configureTextViewMain() {
-        this.textViewMain!!.textSize = 15f
-        this.textViewMain!!.text = "Le premier bien immobilier enregistré vaut "
-    }
-
-    private fun configureTextViewQuantity() {
-        val quantity = Utils.convertDollarToEuro(100).toString()
-        this.textViewQuantity!!.textSize = 20f
-        this.textViewQuantity!!.text = quantity
     }
 
     private fun configureFragment(){
@@ -52,35 +28,17 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.commit()
     }
 
-    private fun configureViewModel() {
-        val mViewModelFactory = Injection().provideViewModelFactory(this)
-        this.itemViewModel = ViewModelProviders.of(this, mViewModelFactory).get(PropertyViewModel::class.java)
-    }
+    private fun configureDetailsPropertyFragment(){
+        var detailsPropertyFragment = supportFragmentManager.findFragmentById(R.id.details_of_the_property_container)
 
-    // Get all properties in database
-    private fun getProperties() {
-        val nameObserver = Observer<List<Property>> { newName ->
-            // Update the UI, in this case, a TextView.
-            val property = newName!![0]
-            val string = "id: "+ property.id+" room: "+ property.numberOfRooms+" prix: "+property.price
-            textViewQuantity!!.text = string
+        if (detailsPropertyFragment == null && details_of_the_property_container != null){
+            val fragmentManager = supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            detailsPropertyFragment = DetailsPropertyFragment()
+            fragmentTransaction.add(R.id.details_of_the_property_container, detailsPropertyFragment)
+            fragmentTransaction.commit()
         }
-
-        this.itemViewModel!!.getProperty().observe(this, nameObserver)
     }
 
-    // Create a new property
-    private fun createProperty(property: Property) {
-        this.itemViewModel!!.createProperty(property)
-    }
 
-    // Delete an property
-    private fun deleteProperty(property: Property) {
-        this.itemViewModel!!.deleteProperty(property.id)
-    }
-
-    // Update an property
-    private fun updateProperty(property: Property) {
-        this.itemViewModel!!.updateItem(property)
-    }
 }
